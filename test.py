140689c9efc05aecd77c872f0741658d19c2e2a3
AnastasiiaMuk <9406533@gmail.com>
2023-05-29 23:21:59 +0200

    Add checking when no card of certificate, adding bad certificate

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 import os
 import time
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 JS_DROP_FILE = "var k=arguments,d=k[0],g=k[1],c=k[2],m=d.ownerDocument||document;for(var e=0;;){var f=d.getBoundingClientRect(),b=f.left+(g||(f.width/2)),a=f.top+(c||(f.height/2)),h=m.elementFromPoint(b,a);if(h&&d.contains(h)){break}if(++e>1){var j=new Error('Element not interactable');j.code=15;throw j}d.scrollIntoView({behavior:'instant',block:'center',inline:'center'})}var l=m.createElement('INPUT');l.setAttribute('type','file');l.setAttribute('multiple','');l.setAttribute('style','position:fixed;z-index:2147483647;left:0;top:0;');l.onchange=function(q){l.parentElement.removeChild(l);q.stopPropagation();var r={constructor:DataTransfer,effectAllowed:'all',dropEffect:'none',types:['Files'],files:l.files,setData:function u(){},getData:function o(){},clearData:function s(){},setDragImage:function i(){}};if(window.DataTransferItemList){r.items=Object.setPrototypeOf(Array.prototype.map.call(l.files,function(x){return{constructor:DataTransferItem,kind:'file',type:x.type,getAsFile:function v(){return x},getAsString:function y(A){var z=new FileReader();z.onload=function(B){A(B.target.result)};z.readAsText(x)},webkitGetAsEntry:function w(){return{constructor:FileSystemFileEntry,name:x.name,fullPath:'/'+x.name,isFile:true,isDirectory:false,file:function z(A){A(x)}}}}}),{constructor:DataTransferItemList,add:function t(){},clear:function p(){},remove:function n(){}})}['dragenter','dragover','drop'].forEach(function(v){var w=m.createEvent('DragEvent');w.initMouseEvent(v,true,true,m.defaultView,0,0,0,b,a,false,false,false,false,0,null);Object.setPrototypeOf(w,null);w.dataTransfer=r;Object.setPrototypeOf(w,DragEvent.prototype);h.dispatchEvent(w)})};m.documentElement.appendChild(l);l.getBoundingClientRect();return l"
 
@@ -19,7 +22,15 @@ button_run_this_project.click()
 button_add = driver.find_element(By.XPATH, '//button[@class="btn btn-primary"]')
 
 
-def add_cert(line_num, cert_file_name):
+def assert_no_card():
+    try:
+        driver.find_element(By.XPATH, '//div[@class="card-body"]//tr[4]/td')
+        assert False
+    except NoSuchElementException:
+        pass
+
+
+def add_cert(cert_file_name):
     button_add.click()
     path = project_root + '\\certs\\' + cert_file_name
     target = driver.find_element(By.XPATH, '//div[@class="card dropbox-panel"]')
@@ -49,15 +60,17 @@ cert1_data = ('Таксер Тест Тестерович', 'UA-22723472', '2015
 cert2_data = ('Лиференко', 'UA-39787008-2015', '2017-12-26 13:13:27 UTC', '2018-12-26 13:13:27 UTC')
 cert3_data = ('UA-00015622-2017', 'UA-00015622-2017', '2017-09-22 07:19:00 UTC', '2027-09-22 07:19:00 UTC')
 
-add_cert(1, 'cert.cer')
+assert_no_card()
+
+add_cert('cert.cer')
 assert_cert(1, cert1_data)
 print('Step 1 passed')
 
-add_cert(2, 'cert2.cer')
+add_cert('cert2.cer')
 assert_cert(2, cert2_data)
 print('Step 2 passed')
 
-add_cert(3, 'czo_2017.cer')
+add_cert('czo_2017.cer')
 assert_cert(3, cert3_data)
 print('Step 3 passed')
 
@@ -86,5 +99,21 @@ print('Step 8 passed')
 get_list_item(3).click()
 assert_cert(3, cert3_data)
 print('Step 9 passed')
+
+button_add = driver.find_element(By.XPATH, '//button[@class="btn btn-primary"]')
+button_add.click()
+time.sleep(1)
+driver.find_element(By.XPATH, '//button[@class="btn btn-secondary"]').click()
+assert_no_card()
+print('Step 10 passed')
+
+add_cert('bad.cer')
+WebDriverWait(driver, 3).until(expected_conditions.alert_is_present())
+time.sleep(1)
+alert = driver.switch_to.alert
+assert alert.text == 'Ошибка при разборе сертификата'
+alert.accept()
+time.sleep(1)
+print('Step 11 passed')
 
 time.sleep(10)
